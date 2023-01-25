d467d9d862bed07a5f3c17d5476a2949e0a938a9
Parvez Siddiqi <parvez@altdigital.tech>
2023-01-25 18:45:03 +0530

    return data

--- services/userService.js
+++ services/userService.js
@@ -2,13 +2,13 @@ const db = require('./db');
 const helper = require('../helper');
 
 async function Login(body){
-//   const sql = "SELECT * FROM users WHERE email = " + body.email + " AND password = " + body.password
-const sql= "SELECT * FROM users WHERE email = 'parvez@gmail.com' AND password = 'Hello@1234'"
-  console.log('sql=> ', sql)
+  //   const sql = "SELECT * FROM users WHERE email = " + body.email + " AND password = " + body.password
+  const sql= "SELECT * FROM users WHERE email = 'parvez@gmail.com' AND password = 'Hello@1234'"
+
   const rows = await db.query(sql);
   const data = helper.emptyOrRows(rows);
 
-  return data
+  return  data && data[0]
 }
 
 module.exports = {
